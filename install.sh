b83b2930af71676cf578ceaea3b7da70a9328bbd
Alistair Bush <alistair.bush@gmail.com>
2017-04-15 12:50:00 +1200

    Improve install and fix font install

--- install.sh
+++ install.sh
@@ -26,6 +26,8 @@ if [[ ! -e "${HOME}/.oh-my-zsh" ]]; then
 
   export MYHOME=${HOME}
 
+  sudo apt-get install zsh
+
   ZSH="" sh -c "$(HOME="${MYHOME}"; ZSH=""; curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
   rm "${HOME}/.zshrc"
@@ -41,17 +43,56 @@ if [[ ! -e "${HOME}/.nvm" ]]; then
   git clone https://github.com/creationix/nvm.git ~/.nvm && cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`
 fi
 
-if [[ -n "$(grep ubuntu /etc/os-release)" && ! -e "${HOME}/.rbenv" ]]; then
-  git clone https://github.com/rbenv/rbenv.git "${HOME}/.rbenv"
-  git clone https://github.com/rbenv/ruby-build.git "${HOME}/.rbenv/plugins/ruby-build"
-  pushd "${HOME}/.rbenv" &> /dev/null
-    src/configure && make -C src
-  popd &> /dev/null
+if [[ -n "$(grep ubuntu /etc/os-release)" ]]; then
+
+  sudo apt-get update
+  sudo apt-get install -y keychain tmux zsh vim-gtk-py2 emacs
+
+  if [[ ! -c "${HOME}/.rbenv" ]]; then
+    git clone https://github.com/rbenv/rbenv.git "${HOME}/.rbenv"
+    git clone https://github.com/rbenv/ruby-build.git "${HOME}/.rbenv/plugins/ruby-build"
+    pushd "${HOME}/.rbenv" &> /dev/null
+      src/configure && make -C src
+    popd &> /dev/null
+  fi
+
+  sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo apt-key fingerprint 0EBFCD88
+  sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+  echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+
+  sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
+  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
+
+  sudo add-apt-repository multiverse
+
+  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+
+  echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+
+  sudo apt-get update
+  sudo apt-get install -y docker-ce dotnet-dev-1.0.1 chromium-browser spotify-client steam
+
+  curl -sSL https://get.haskellstack.org/ | sh
 
   if [[ ! -e "${HOME}/.fonts" ]]; then
     TMP_DIR="$(mktemp -d)"
     curl -L -o "${TMP_DIR}/sourcecodepro.zip" https://github.com/adobe-fonts/source-code-pro/archive/2.030R-ro/1.050R-it.zip
-    unzip "${TMP_DIR}/sourcecodepro.zip"
+    unzip "${TMP_DIR}/sourcecodepro.zip" -d "${TMP_DIR}"
+    rm "${TMP_DIR}/sourcecodepro.zip"
+    mkdir -p "${HOME}/.fonts"
+    mv "${TMP_DIR}"/* "${HOME}/.fonts/"
+    sudo fc-cache -f -v
   fi
 fi
 
